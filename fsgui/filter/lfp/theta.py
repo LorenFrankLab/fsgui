5f517c004e10470ffed237e23d62cc74f4432460
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2023-02-09 10:03:33 -0800

    Add theta implementation

--- fsgui/filter/lfp/theta.py
+++ fsgui/filter/lfp/theta.py
@@ -2,9 +2,6 @@ import multiprocessing as mp
 import numpy as np
 import fsgui.process
 import fsgui.node
-import json
-import shapely
-
 
 class ThetaFilterType(fsgui.node.NodeTypeObject):
     def __init__(self, type_id):
@@ -18,7 +15,6 @@ class ThetaFilterType(fsgui.node.NodeTypeObject):
                 'instance_id': '',
                 'nickname': 'Theta filter',
                 'source_id': None,
-                'speedThresh': 2,
                 'filterDelay': 730,
                 'thetaPhase': 0,
                 # 'mode': 'live',
@@ -54,16 +50,6 @@ class ThetaFilterType(fsgui.node.NodeTypeObject):
                 'tooltip': 'Source to receive LFP data',
             },
             {
-                'label': 'Speed threshold',
-                'name': 'speedThresh',
-                'type': 'double',
-                'lower': 0,
-                'upper': 100,
-                'decimals': 2,
-                'default': config['speedThresh'],
-                'units': 'cm/s',
-            },
-            {
                 'label': 'Filter Delay',
                 'name': 'filterDelay',
                 'type': 'integer',
@@ -82,3 +68,80 @@ class ThetaFilterType(fsgui.node.NodeTypeObject):
                 'units': 'deg',
             }
         ]
+
+class ThetaFilterParams:
+    def __init__(self, targetPhase):
+        self.targetPhase = targetPhase
+
+class ThetaFilterCoefficientsDefault:
+    def __init__(self):
+        self.length = 3
+        self.numerator = np.array([
+            0.0165,
+            0.0,
+            -0.0165,
+        ], dtype='double')
+        self.denominator = np.array([
+            1,
+            -1.9662,
+            0.9670,
+        ], dtype='double')
+
+class ThetaFilter:
+    def __init__(self, coefficients):
+        self.coefficients = coefficients
+
+        # current filter state
+        self.z = np.zeros(shape=(self.coefficients.length,), dtype='double')
+        
+        # The last filtered LFP value
+        self.fLFPLast = 0
+
+        # the time of the last upward going zero crossing
+        self.upZeroCrossLast = -1e100
+        # the current estimate of the period in seconds
+        self.periodEstimate = 1e100
+
+        # time, in seconds, for next stimulation
+        self.nextTrigger = None
+
+        # this is using a negative cosine wave as reference
+        if self.params.targetPhase < 90:
+            self.useUpCross = False
+            self.degFromZeroCross = self.params.targetPhase + 90
+        elif self.params.targetPhase >= 90 and self.params.targetPhase < 270:
+            self.useUpCross = True
+            self.degFromZeroCross = self.params.targetPhase - 90
+        elif self.params.targetPhase >= 270:
+            self.useUpCross = False
+            self.degFromZeroCross = self.params.targetPhase - 270
+        else:
+            raise ValueError(f'Invalid target phase: {self.params.targetPhase}')
+
+    def filter_data(self, lfp):
+        fLFP = self.coefficients.numerator[0] * lfp + self.z[0]
+        for i in range(1, self.coefficients.length):
+            # update the filter state for the next iteration
+            self.z[i-1] = self.coefficients.numerator[i] * lfp + self.z[i] - self.coefficients.denominator[i]*fLFP
+        return fLFP
+    
+    def process_theta_data(lfpVal, sampleTime):
+        fLFPCurrent = self.filter_data(lfpVal)
+
+        if fLFPCurrent >= 0 and self.fLFPLast < 0:
+            # signal rising edge, zero crossing
+            upZeroCross = sampleTime
+            self.periodEstimate = upZeroCross - self.upZeroCrossLast
+            if self.useUpCross:
+                self.nextTrigger = sampleTime + (self.degFromZeroCross/360.0)*self.periodEstimate
+            self.upZeroCrossLast = upZeroCross
+        elif fLFPCurrent <= 0 and self.fLFPLast > 0:
+            if not self.useUpCross:
+                self.nextTrigger = sampleTime + (self.degFromZeroCross/360.0)*self.periodEstimate
+        
+        self.fLFPLast = fLFPCurrent
+
+        if self.nextTrigger is not None:
+            return self.nextTrigger <= sampleTime
+        else:
+            return False
