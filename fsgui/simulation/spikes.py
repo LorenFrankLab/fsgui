813b30c43556df3cd8e0e8e602a79570f1cd0c58
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2023-02-26 11:46:38 -0800

    Improve spikes simulation to be based on position

--- fsgui/simulation/spikes.py
+++ fsgui/simulation/spikes.py
@@ -17,6 +17,8 @@ class SpikesGeneratorType(fsgui.node.NodeTypeObject):
                 'type_id': type_id,
                 'instance_id': '',
                 'nickname': name,
+                'covariate_source': None,
+                'n_simulated_neurons': 12,
             }
         )
 
@@ -42,13 +44,33 @@ class SpikesGeneratorType(fsgui.node.NodeTypeObject):
                 'default': config['nickname'],
                 'tooltip': 'This is the name the source is displayed as in menus.',
             },
+            {
+                'label': 'Covariate source',
+                'name': 'covariate_source',
+                'type': 'node:bin_id',
+                'default': config['covariate_source'],
+                'tooltip': 'This node provides a position to affect firing.',
+            },
+            {
+                'label': 'Number of simulated neurons',
+                'name': 'n_simulated_neurons',
+                'type': 'integer',
+                'lower': 1,
+                'upper': 64,
+                'default': config['n_simulated_neurons'],
+                'units': 'neurons',
+                'tooltip': 'Number of neurons to simulate.'
+
+            }
         ]
     
-    def build(self, config, param_map):
+    def build(self, config, addr_map):
+        covariate_address=addr_map[config['covariate_source']]
+
         waveform_length = 40 # samples in a waveform
         n_tetrodes = 4 # number of waveforms
 
-        n_simulated_neurons = 12
+        n_simulated_neurons = config['n_simulated_neurons']
 
         low = -5
         high = 5
@@ -68,16 +90,29 @@ class SpikesGeneratorType(fsgui.node.NodeTypeObject):
         samples = -action_potential(x)
         waveform_schema = samples / np.max(samples)
 
+        def transform_range(values, source_range, target_range):
+            return np.interp(values, source_range, target_range)
+
         def setup(connection, data):
+            data['covariate_sub'] = fsgui.network.UnidirectionalChannelReceiver(covariate_address)
             data['neurons'] = [
                 np.array([random.randint(low, high) for ch in range(n_tetrodes)])
                 for n in range(n_simulated_neurons)
             ]
 
             data['start_time'] = time.time()
+            data['position'] = 0
+
+            data['position_map'] = {}
 
         def workload(connection, publisher, reporter, data):
-            neuron_id = random.randint(0, n_simulated_neurons-1)
+            covariate_value = data['covariate_sub'].recv(timeout=0)
+            if covariate_value is not None:
+                data['position'] = covariate_value
+
+            # create a mapping so each position corresponds to firing one neuron
+            neuron_id = data['position_map'].setdefault(data['position'], int(transform_range(data['position'], [0,20], [0, n_simulated_neurons])) % n_simulated_neurons)
+
             neuron = data['neurons'][neuron_id]
             mark = np.random.multivariate_normal(neuron, covariance_matrix)
             waveforms = [(waveform_schema * peak).tolist() for peak in mark]
@@ -94,9 +129,10 @@ class SpikesGeneratorType(fsgui.node.NodeTypeObject):
 
             publisher.send(value)
             reporter.send({
-                'spike': np.max(waveforms)
+                'spike': np.max(waveforms),
+                'neuron_id': neuron_id,
             })
 
-            time.sleep(0.0006)
+            time.sleep(0.01)
         
         return fsgui.process.build_process_object(setup, workload)
