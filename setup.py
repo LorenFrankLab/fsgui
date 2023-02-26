48615781129ce967d0e38379debc4afebe26d8ec
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2023-02-26 12:35:58 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='fsgui',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'oyaml',
+        'numpy',
+        'zmq',
+        'shapely',
+        'msgpack',
+        'PyQt6',
+        'matplotlib',
+        'h5py',
+        'graphviz',
+        'pyqtgraph',
+        'pyopengl',
+    ],
+)
