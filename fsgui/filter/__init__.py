a3ba7f42e4b065a620d5ae902939197f1313d9f7
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2023-03-26 20:34:02 -0700

    Reorganize filter imports

--- fsgui/filter/__init__.py
+++ fsgui/filter/__init__.py
@@ -1,9 +1,9 @@
-from fsgui.filter.lfp.ripple import *
+import fsgui.filter.lfp.ripple
 import fsgui.filter.lfp.ripple_new
-from fsgui.filter.lfp.theta import *
-from fsgui.filter.lfp.theta_hilbert import *
-from fsgui.filter.spatial.polygon import *
-from fsgui.filter.spatial.rectangle import *
+import fsgui.filter.lfp.theta
+import fsgui.filter.lfp.theta_hilbert
+import fsgui.filter.spatial.polygon
+import fsgui.filter.spatial.rectangle
 import fsgui.filter.spatial.speed
 import fsgui.filter.spikes.markspace
 import fsgui.filter.cluster
@@ -12,12 +12,12 @@ import fsgui.filter.arm
 class FilterProvider:
     def get_nodes(self):
         return [
-            AxisAlignedRectangleFilterType('axis-aligned-rect-filter-type'),
-            GeometryFilterType('geometry-filter-type'),
+            fsgui.filter.spatial.rectangle.AxisAlignedRectangleFilterType('axis-aligned-rect-filter-type'),
+            fsgui.filter.spatial.polygon.GeometryFilterType('geometry-filter-type'),
             fsgui.filter.spatial.speed.SpeedFilterType('speed-filter-type'),
-            RippleFilterType('ripple-filter-type'),
+            fsgui.filter.lfp.ripple.RippleFilterType('ripple-filter-type'),
             fsgui.filter.lfp.ripple_new.RippleFilterType('ripple-new-filter-type'),
-            ThetaFilterType('theta-filter-type'),
+            fsgui.filter.lfp.theta.ThetaFilterType('theta-filter-type'),
             fsgui.filter.lfp.theta_hilbert.ThetaPhaseHilbertFilterType('theta-phase-hilbert-filter-type'),
             fsgui.filter.spikes.markspace.MarkSpaceEncoderType('mark-space-encoder-type'),
             fsgui.filter.cluster.DecoderType('point-process-encoder-type'),
